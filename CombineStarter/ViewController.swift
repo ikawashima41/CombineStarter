316bb05c940e5875746d5b716bf9f06092f41cbd
ikawashima41 <i.kawashima41@gmail.com>
2020-01-10 23:38:12 +0900

    Initial Commit

---
+++ CombineStarter/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CombineStarter
+//
+//  Created by Iichiro Kawashima on 2020/01/10.
+//  Copyright © 2020 Iichiro Kawashima. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
